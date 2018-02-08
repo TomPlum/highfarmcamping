f6eb85ea384a71da9cbaac85027696854284f6d4
Tom Plumpton <Thomas.Plumpton@hotmail.co.uk>
2018-02-08 22:12:15 +0000

    init commit
    -Started User Interface Design
    -Added Relevant Pages
    -Added DateTime to Nav

---
+++ public/js/dashboard.js
@@ -0,0 +1,58 @@
+function getToday() {
+    const today = new Date();
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let YYYY = today.getFullYear(); //YYYY
+    let day = today.getDay(); //0-6 (0 is Sunday)
+    let date = today.getDate(); //1-31
+    let month = today.getMonth(); //0-11
+    console.log(day);
+
+    return days[day] + " " + date + getDateSuffix(date) + " " + months[month] + " " + YYYY;
+}
+
+function getDateSuffix(number) {
+    if (number > 3 && number < 21) {
+        return "th";
+    } else if (number === 1) {
+        return "st";
+    } else if (number === 2) {
+        return "nd";
+    } else if (number === 3) {
+        return "rd";
+    } else {
+        switch (number % 10) {
+            case 1:
+                return "st";
+            case 2:
+                return "nd";
+            case 3:
+                return "rd";
+            default:
+                return "th";
+        }
+    }
+}
+
+function updateTime() {
+    const today = new Date();
+    let HH = today.getHours();
+    if (HH < 10) {
+        HH = "0" + HH;
+    }
+    let MM = today.getMinutes();
+    if (MM < 10) {
+        MM = "0" + MM;
+    }
+    let SS = today.getSeconds();
+    if (SS < 10) {
+        SS = "0" + SS;
+    }
+
+    $('#current-time').html(HH + ":" + MM + ":" + SS);
+}
+
+$(document).ready(function() {
+   $('#today').html(getToday());
+   setInterval(updateTime, 1000);
+});
