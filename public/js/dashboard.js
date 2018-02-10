6d56109ebff629420e9764e83bf48bce251a31cf
VFBPower1 <31651492+VFBPower1@users.noreply.github.com>
2018-02-10 19:37:50 +0000

    Revert "Revert "Merge branch 'master' of https://github.com/TomPlum/camping""
    This reverts commit 1031099da491cdbae045c191adae991a3f581d7c.

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,3 +1,5 @@
+console.log("Loaded dashboard.js");
+
 function getToday() {
     const today = new Date();
     const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
