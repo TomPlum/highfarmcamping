dba659596e7a525d963f5cfd21efe0adb0092aea
TomPlum <Thomas.Plumpton@hotmail.co.uk>
2018-02-13 16:17:39 +0000

    added test example from Tuesday

---
+++ db/query.js
@@ -0,0 +1,7 @@
+//TODO CREATE Function
+
+//TODO READ Function
+
+//TODO UPDATE Function
+
+//TODO DELETE Function
--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,13 @@ const mysql = require('../db/mysql');
 
 /* GET Home Page */
 router.get('/', function(req, res) {
-    mysql.connection.query("SELECT * FROM test", (err, rows, fields) => {
+    mysql.connection.query("SELECT * FROM bookings", (err, rows, fields) => {
+        if (err) {
+            console.log(err);
+        }
         console.log(rows);
         console.log(fields);
+
     });
     res.render('dashboard', {title: 'Dashboard'});
 });
@@ -20,5 +24,34 @@ router.get('/book', function(req, res) {
 router.get('/help', function(req, res) {
     res.render('help', {title: 'Information'});
 });
+
+/*GET Test Page*/
+router.get('/test', function(req, res) {
+    res.render('test', {title: "Testing page"});
+});
+
+/* POST Test page */
+router.post('/send-to-database', function(req, res) {
+   let test_field = req.body.test_field;
+   let values = [
+       ['Tom'],
+       ['Vincent'],
+       ['Flo'],
+       ['Jack']
+   ];
+   mysql.connection.query("INSERT INTO test (test_field) VALUES ?", [values], function(err, result) {
+       if (err) {
+           console.log(err);
+       } else {
+           console.log("Affected " + result.affectedRows + " rows.");
+       }
+   });
+});
+
+function createSqlString(dbname, columns, data) {
+    return mysql.connection.query("INSERT INTO ? (?) VALUES ?", [dbname, columns data], function(err, result) {
+
+    });
+}
 
 module.exports = router;
