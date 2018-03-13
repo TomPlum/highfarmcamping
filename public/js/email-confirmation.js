874b43cad465af0971baa10d61371a1a0d419827
vincentheimburg <31849595+vincentheimburg@users.noreply.github.com>
2018-03-13 16:50:53 +0000

    Email confirmation
    Just alpha stage rn. Does not work. yet. Svenja and I will look further into it

--- public/js/email-confirmation.js
+++ public/js/email-confirmation.js
@@ -1,8 +1,55 @@
+$(document).ready(() => {
+
+});
+
+var customerEmail;
+
+$('#printPDF').click(function(){
+    getCustomerFromDB();
+    sendEmailConfirmation();
+})
+
+function getCustomerFromDB() {
+    $.ajax({
+        type: 'POST',
+        async: false,
+        url: '/get-customer',
+        data: {
+            'ID': window.location.search.substring(12)
+        },
+        success: function (data) {
+            stopLoadingAnimation();
+            customer=data;
+            console.log(customer);
+            customerEmail = customer[0].email_address;
+            console.log(customerEmail);
+        },
+        error: function () {
+            console.log("We have an error in AJAX request for customer ID: " + err);
+            alert(errorNotification);
+        }
+    });
+}
+
+function createPdfFromHtml() {
+    var fs = require('fs');
+    var pdf = require('html-pdf');
+    var html = fs.readFileSync('./test/businesscard.html', 'utf8');
+    var options = {format: 'A4', orientation: 'landscape'};
+
+    pdf.create(html, options).toFile('./businesscard.pdf', function (err, res) {
+        if (err) return console.log('Error creating the PDF');
+        console.log(res); // { filename: '/app/businesscard.pdf' }
+    });
+
+}
+
+
 function sendEmailConfirmation() {
 
-    var nodemailer = require('nodemailer');
+    let nodemailer = require('nodemailer');
 
-    var transporter = nodemailer.createTransport({
+    let transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
             user: 'highfarm.campsites@gmail.com',
@@ -10,12 +57,13 @@ function sendEmailConfirmation() {
         }
     });
 
-    var mailOptions = {
+    let mailOptions = {
         from: 'High Farm Campsites <highfarm.campsites@gmail.com>',
-        to: 'heimburg@outlook.com',
+        to: customerEmail,
         subject: 'High Farm Campground - Booking Overview',
         text: 'Test Text. That was easy.'
     }
+
 
     transporter.sendMail(mailOptions, function (error, info) {
         if (error) {
